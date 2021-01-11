d9c88f5d903345c83393924593c059e466f3d346
jforbis <forbisjohn@gmail.com>
2021-01-10 20:58:39 -0600

    save point: finished query 1

--- PH_emp_db.sql
+++ PH_emp_db.sql
@@ -63,6 +63,10 @@ SELECT * FROM employees
 SELECT * FROM salaries
 SELECT * FROM titles
 
---Queries
-
-
+-- Queries
+-- Query #1
+SELECT e.emp_no, e.last_name, e.first_name, e.sex,
+s.salary
+FROM employees e
+INNER JOIN salaries s ON
+e.emp_no = s.emp_no;
