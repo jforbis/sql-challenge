2aa298a899ca85e977e1fd1731ac562508f8110f
jforbis <forbisjohn@gmail.com>
2021-01-10 22:08:01 -0600

    save point: updating notes

--- PH_emp_db.sql
+++ PH_emp_db.sql
@@ -64,18 +64,21 @@ SELECT * FROM salaries
 SELECT * FROM titles
 
 -- Queries
--- Query #1
+-- Query #1. List the following details of each employee: 
+-- 			 employee number, last name, first name, sex, and salary.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex,
 s.salary
 FROM employees e
 INNER JOIN salaries s ON
 e.emp_no = s.emp_no;
 
--- Query #2
+-- Query #2. List first name, last name, and hire date for employees who were hired in 1986.
 SELECT last_name, first_name, hire_date FROM employees
 WHERE hire_date >= '1986-1-1' AND hire_date <= '1986/12/31';
 
--- Query #3
+-- Query #3. List the manager of each department with the following information: 
+-- 			 department number, department name, the manager's employee number, 
+-- 			 last name, first name.
 SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 FROM dept_manager dm
 INNER JOIN departments d ON
@@ -83,10 +86,24 @@ dm.dept_no = d.dept_no
 INNER JOIN employees e ON
 dm.emp_no = e.emp_no;
 
--- Query #4 (only getting 24 lines - why?)
+-- Query #4. List the department of each employee with the following information: 
+-- 			 employee number, last name, first name, and department name.
+-- (only getting 24 lines - why?)
 SELECT d.dept_name, e.emp_no, e.last_name, e.first_name
 FROM employees e
 INNER JOIN dept_manager dm ON
 e.emp_no = dm.emp_no
 INNER JOIN departments d ON
 dm.dept_no = d.dept_no;
+
+-- Query #5. List first name, last name, and sex for employees whose first name
+-- 			 is "Hercules" and last names begin with "B."
+
+-- Query #6. List all employees in the Sales department, including their 
+-- 			 employee number, last name, first name, and department name.
+
+-- Query #7. List all employees in the Sales and Development departments, including 
+-- 			 their employee number, last name, first name, and department name.
+
+-- Query #8. In descending order, list the frequency count of employee last names, 
+-- 			 i.e., how many employees share each last name.
