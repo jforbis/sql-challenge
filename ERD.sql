441a207f5da9db8246fc964f5c05be0a1d0a750b
jforbis <forbisjohn@gmail.com>
2021-01-10 18:50:52 -0600

    save point: Imported my CSVs but having an error on my dept_manager table when trying to import

--- ERD.sql
+++ ERD.sql
@@ -2,7 +2,6 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/2ZIEFA
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-DROP TABLE employees CASCADE;
 
 CREATE TABLE "employees" (
     "emp_no" Integer   NOT NULL,
@@ -12,46 +11,57 @@ CREATE TABLE "employees" (
     "last_name" Varchar   NOT NULL,
     "sex" Varchar   NOT NULL,
     "hire_date" Date   NOT NULL,
-   	PRIMARY KEY (emp_no),
-	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "dept_emp" (
     "emp_no" Integer   NOT NULL,
-    "dept_no" Varchar   NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+    "dept_no" Varchar   NOT NULL
 );
 
 CREATE TABLE "salaries" (
     "emp_no" Integer   NOT NULL,
-    "salary" Integer   NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+    "salary" Integer   NOT NULL
 );
 
 CREATE TABLE "dept_manager" (
     "emp_no" Integer   NOT NULL,
-    "dept_no" Varchar   NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+    "dept_no" Varchar   NOT NULL
 );
 
 CREATE TABLE "titles" (
     "title_id" Varchar   NOT NULL,
     "title" Varchar   NOT NULL,
-	PRIMARY KEY (title_id)
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
 );
 
 CREATE TABLE "departments" (
     "dept_no" Varchar   NOT NULL,
     "dept_name" Varchar   NOT NULL,
-	PRIMARY KEY (dept_no)
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
 );
 
-SELECT * FROM departments
-SELECT * FROM dept_emp
-SELECT * FROM dept_manager
-SELECT * FROM employees
-SELECT * FROM salaries
-SELECT * FROM titles
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
