66295e10328ecc17b49dae4566a76e09e3b8f265
jforbis <forbisjohn@gmail.com>
2021-01-10 22:13:29 -0600

    save point: finished query #5

--- PH_emp_db.sql
+++ PH_emp_db.sql
@@ -98,6 +98,9 @@ dm.dept_no = d.dept_no;
 
 -- Query #5. List first name, last name, and sex for employees whose first name
 -- 			 is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex FROM employees
+Where first_name = 'Hercules'
+AND LEFT(last_name, 1) = 'B'
 
 -- Query #6. List all employees in the Sales department, including their 
 -- 			 employee number, last name, first name, and department name.
