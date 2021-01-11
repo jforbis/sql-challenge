3e649fb36db88a9bdcc044672c50a2c998879dd9
jforbis <forbisjohn@gmail.com>
2021-01-10 18:31:58 -0600

    save point: imported all tables but department_manager...have an error when trying to import it

--- ERD.sql
+++ ERD.sql
@@ -48,5 +48,10 @@ CREATE TABLE "departments" (
 	PRIMARY KEY (dept_no)
 );
 
+SELECT * FROM departments
+SELECT * FROM dept_emp
+SELECT * FROM dept_manager
 SELECT * FROM employees
+SELECT * FROM salaries
+SELECT * FROM titles
 
